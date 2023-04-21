fc595e16cc185fae48091cc85a576d9c02052db0
Andrea Sierra <andrea.sierra05@uptc.edu.co>
2023-04-21 17:08:12 -0500

    Fix test case

--- src/controllers/Controller.java
+++ src/controllers/Controller.java
@@ -82,7 +82,7 @@ public class Controller implements ActionListener, KeyListener {
         }
     }
     private void p(){
-        processManager.p();
+        processManager.printReport();
     }
 
     private void cleanMainTableProcess(){
